33b9a0632341ddeb327a6cd91f6076d8d16d80af
Immanuela Belaineh <54345155+belainehi@users.noreply.github.com>
2020-08-27 23:35:33 -0400

    Sign up authentication part completed.

--- app/src/main/java/com/Team/volunteer_info/RegisterActivity.java
+++ app/src/main/java/com/Team/volunteer_info/RegisterActivity.java
@@ -1,15 +1,22 @@
 package com.Team.volunteer_info;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -43,9 +50,13 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "All fields required!", Toast.LENGTH_SHORT).show();
 
                 }
-                else
+                else if (new_password.getText().toString().equals(confirm_password.getText().toString()))
                 {
                     createNewUser();
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "Passwords must match!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -53,6 +64,24 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void createNewUser() {
+        Task<AuthResult> authResultTask = mAuth.createUserWithEmailAndPassword(user_email.getText().toString(), new_password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("RegisterActivity", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("RegisterActivity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed. You may have an account already.", Toast.LENGTH_SHORT).show();
+                        }
 
+                        // ...
+                    }
+                });
     }
 }
