d999d8621d37ed61ebfd770024ec52183b07eccb
denisedaniels <denise_daniels@bloomfield.edu>
2020-08-29 02:58:17 -0400

    Fixed Map Issues

--- app/src/main/java/com/Team/volunteer_info/MapsActivity.java
+++ app/src/main/java/com/Team/volunteer_info/MapsActivity.java
@@ -2,23 +2,55 @@ package com.Team.volunteer_info;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.Team.volunteer_info.fragments.FragmentFeed;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
     private GoogleMap mMap;
+    Button mapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
+        mapBtn=findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -32,5 +64,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng bloomfield = new LatLng(40.8068, -74.1854);
         mMap.addMarker(new MarkerOptions().position(bloomfield).title("Bloomfield, NJ"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(bloomfield));
+
     }
 }
